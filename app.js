39ae08f6129f1d9d52f014699fab42ff8896041d
andrewflower-rs <arnaldomahone@hotmail.com>
2024-02-19 21:34:15 -0300

    empiezo el desafio

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3030;
+
+/* CONFIGS */
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+/* MIDDLEWARE */
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+})
+
+/* SERVER */
+app.listen(port, () => console.log('http://localhost:3030'));
